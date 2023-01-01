0e7bbb4b1187eec21d333843560dfbe59c4bb410
Arson Marianus <33475446+m-arson@users.noreply.github.com>
2023-01-01 23:58:39 +0700

    Update get_memory_usage.sh

--- get_memory_usage.sh
+++ get_memory_usage.sh
@@ -1,1 +1,5 @@
+# total_free   = total - free + buffer + cache
+# memory_usage = (total-total_free)/total * 100
+
+
 free -m | grep Mem | xargs | cut -d" " -f2,3,6 | awk '{res=$1-$2+$3; d=($1-res)/$1*100}{printf "Memory Usage : %.2f %%\n", d}'
