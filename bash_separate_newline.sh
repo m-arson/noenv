521626059d846625fa6b959069111629c328d228
Arson Marianus <33475446+m-arson@users.noreply.github.com>
2023-02-25 21:51:03 +0700

    Update bash_separate_newline.sh

--- bash_separate_newline.sh
+++ bash_separate_newline.sh
@@ -1,1 +1,9 @@
 IFS=$'\n' data=(`find /home/arson/Downloads/* -prune -type f`); for d in ${data[@]}; do echo $d; done
+
+OR
+
+IFS=, read -ra dataa < crossbone.txt; echo ${dataa[0]}
+
+OR
+
+readarray -t my_array < <(sed ':a;N;$!ba;s/\n/,/g' data.txt)
